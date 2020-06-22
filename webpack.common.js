93bce06f356c3e2ffe831b113b2a5f33e62a5f40
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-06-22 14:23:36 -0800

    v1.2 add jsx & html loaders

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,15 @@
     output: {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
+      // the filename template for entry chunks
+      publicPath: "/assets/", // string
+      // the url to the output directory resolved relative to the HTML page
+      library: "MyLibrary", // string,
+      // the name of the exported library
+      libraryTarget: "umd", // universal module definition
+      // the type of the exported library
+      /* Advanced output configuration (click to show) */
+      /* Expert output configuration (on own risk) */
     },
     resolve: {
       alias: {
@@ -65,6 +74,46 @@
     ],
     module: {
       rules: [
+      //   {
+      //   test: /\.jsx?$/,
+      //   include: [
+      //     path.resolve(__dirname, "app")
+      //   ],
+      //   exclude: [
+      //     path.resolve(__dirname, "app/demo-files")
+      //   ],
+      //   // these are matching conditions, each accepting a regular expression or string
+      //   // test and include have the same behavior, both must be matched
+      //   // exclude must not be matched (takes preferrence over test and include)
+      //   // Best practices:
+      //   // - Use RegExp only in test and for filename matching
+      //   // - Use arrays of absolute paths in include and exclude
+      //   // - Try to avoid exclude and prefer include
+      //   issuer: { test, include, exclude },
+      //   // conditions for the issuer (the origin of the import)
+      //   enforce: "pre",
+      //   enforce: "post",
+      //   // flags to apply these rules, even if they are overridden (advanced option)
+      //   loader: "babel-loader",
+      //   // the loader which should be applied, it'll be resolved relative to the context
+      //   options: {
+      //     presets: ["es2015"]
+      //   },
+      //   // options for the loader
+      // },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     // apply multiple loaders and options
+      //     "htmllint-loader",
+      //     {
+      //       loader: "html-loader",
+      //       options: {
+             
+      //       }
+      //     }
+      //   ]
+      // },
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
